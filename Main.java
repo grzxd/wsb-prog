bcd1877a9b0c0cbbbe36be8ccf12fc96b1c7cbb0
grzxd <grzysiek.xd@gmail.com>
2024-10-12 11:59:30 +0200

    - Add Hello world

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        int a=125;
+        System.out.println(a);
+        int wynik=a/2;
+        System.out.println(wynik);
     }
 }

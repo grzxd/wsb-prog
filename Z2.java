8e46f33cd32278718baaad41cb356dfa1cd62b85
grzxd <grzysiek.xd@gmail.com>
2024-10-26 12:21:47 +0200

    2/6

--- Z2.java
+++ Z2.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Z2 {
     public static void main(String[] args) {
         boolean todo=true;
-        int l1 = 0, l2 = 0, l3 = 0;
+        int l1 = 0, l2 = 0, l3 = 0, max, min;
 
         System.out.println("Podaj 3 liczby całkowite");
         while (todo) {
@@ -22,28 +22,28 @@ public class Z2 {
 
         if (l1>l2) {
             if (l1>l3) {
-                System.out.println("Największą liczbą jest " + l1);
+                max = l1;
                 if (l3>l2) {
-                    System.out.println("Najmniejszą liczbą jest " + l2);
+                    min = l2;
                 } else {
-                    System.out.println("Najmniejszą liczbą jest " + l3);
+                    min = l3;
                 }
             } else {
-                System.out.println("Największą liczbą jest " + l3);
-                System.out.println("Najmniejszą liczbą jest " + l2);
+                max = l3;
+                min = l2;
+            }
+        } else if (l2>l3) {
+            max = l2;
+            if (l3 > l1) {
+                min = l1;
+            } else {
+                min = l3;
             }
         } else {
-            if (l2>l3) {
-                System.out.println("Największą liczbą jest " + l2);
-                if (l3 > l1) {
-                    System.out.println("Najmniejszą liczbą jest " + l1);
-                } else {
-                    System.out.println("Najmniejszą liczbą jest " + l3);
-                }
-            } else {
-                System.out.println("Największą liczbą jest " + l3);
-                System.out.println("Najmniejszą liczbą jest " + l1);
-            }
+            max = l3;
+            min = l1;
         }
+        System.out.println("Największą liczbą jest " + max);
+        System.out.println("Najmniejszą liczbą jest " + min);
     }
 }

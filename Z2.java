62a0b545b841e7bdde1f75dc6c3b85d3aabeaacc
grzysiek <grzysiek.xd@gmail.com>
2024-11-09 08:22:34 +0100

    refactor of nested if's

--- Z2.java
+++ Z2.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Z2 {
     public static void main(String[] args) {
         boolean todo=true;
-        int l1 = 0, l2 = 0, l3 = 0, max, min;
+        int l1 = 0, l2 = 0, l3 = 0, max=0, min=0;
 
         System.out.println("Podaj 3 liczby całkowite");
         while (todo) {
@@ -20,29 +20,27 @@ public class Z2 {
             }
         }
 
-        if (l1>l2) {
-            if (l1>l3) {
-                max = l1;
-                if (l3>l2) {
-                    min = l2;
-                } else {
-                    min = l3;
-                }
-            } else {
-                max = l3;
-                min = l2;
-            }
-        } else if (l2>l3) {
+        if (l1 > l2 && l1 > l3) {
+            max = l1;
+        }
+        if (l2 > l1 && l2 > l3) {
             max = l2;
-            if (l3 > l1) {
-                min = l1;
-            } else {
-                min = l3;
-            }
-        } else {
+        }
+        if (l3 > l1 && l3 > l2) {
             max = l3;
+        }
+
+        if (l1<l2 && l1<l3) {
             min = l1;
         }
+        if (l2<l1 && l2<l3) {
+            min = l2;
+        }
+        if (l3<l1 && l3<l2) {
+            min = l3;
+        }
+
+
         System.out.println("Największą liczbą jest " + max);
         System.out.println("Najmniejszą liczbą jest " + min);
     }
